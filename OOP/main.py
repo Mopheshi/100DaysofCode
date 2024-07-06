40026ee8211a286a583f2d9123d07fadd8f13350
Mophe <87334468+Mopheshi@users.noreply.github.com>
2024-07-06 12:30:13 +0100

    Coffee Machine with OOP

---
+++ OOP/main.py
@@ -0,0 +1,35 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menus = Menu()
+menuItems = menus.get_items().split("/")  # ['latte', 'espresso', 'cappuccino', ''] or you can the following:
+menuItem = [item for item in menuItems]  # ['latte', 'espresso', 'cappuccino', ''] the same as above.
+
+coffeeMaker = CoffeeMaker()
+moneyMachine = MoneyMachine()
+
+off = False
+print(menuItems)
+
+while not off:
+    request = input(f"What would you like? ({menus.get_items()} 🍵☕?): ")
+
+    if request in menuItem:
+        coffee = menus.find_drink(request)
+        cost = coffee.cost
+
+        if coffeeMaker.is_resource_sufficient(coffee):
+            if moneyMachine.make_payment(cost):
+                coffeeMaker.make_coffee(coffee)
+
+    elif request == 'off':
+        print("Shutting down...")
+        off = True
+
+    elif request == 'report':
+        coffeeMaker.report()
+        moneyMachine.report()
+
+    else:
+        print("Invalid input! Please try again...")
