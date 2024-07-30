d2886680208b22ce66c8915a9c6f4fbe90a5d7b3
Mophe <87334468+Mopheshi@users.noreply.github.com>
2024-07-30 10:41:39 +0100

    Turtle Snake (End of Day 20)...

--- Turtle Snake/main.py
+++ Turtle Snake/main.py
@@ -1,26 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.title('Snake')
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.tracer(8, 25)
+screen.tracer(0)  # Turn off the screen updates
 
-num_snakes = 3
+snake = Snake()
 
-snakes = list()
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
-for i in range(num_snakes):
-    s = Turtle('square')
-    s.color('white')
-    s.penup()
-    s.goto(x=0 + i * -20, y=0)
-    snakes.append(s)
-
-gameOver = False
-
-while not gameOver:
-    for snake in snakes:
-        snake.forward(20)
+while not snake.game_over:
+    snake.refresh()
+    snake.move()
 
 screen.exitonclick()
---
+++ Turtle Snake/snake.py
@@ -0,0 +1,87 @@
+from turtle import Turtle, Screen
+import time
+
+
+class Snake:
+    """
+    A Snake class with three circles and color them white at the center of the screen with a gap of 20
+    pixels between each circle to the left of the previous circle in the snake object list and append each circle
+    to the snake object list to keep track of the snake's position and movement on the screen when the snake
+    moves forward by 20 pixels each time the loop runs and delay for 0.1 seconds to update the screen to show the
+    snake moving forward.
+    """
+
+    def __init__(self):
+        """
+        Initialize the snake object with three circles and color them white
+        """
+        self.snakes = list()
+        self.create_snake()
+        self.game_over = False
+
+    def create_snake(self):
+        """
+        Create a snake object with three circles and color them white at the center of the screen with a gap of 20
+        pixels between each circle.
+        :return:
+        """
+        for i in range(3):
+            s = Turtle('circle')
+            s.color('white')
+            s.penup()
+            s.goto(x=0 + i * -20, y=0)  # Move the snake to the left by 20 pixels each time
+            self.snakes.append(s)
+
+    def move(self):
+        """
+        Move the snake forward by 20 pixels
+        :return:
+        """
+        # Start from the last snake and move backwards to the first snake
+        for snake in range(len(self.snakes) - 1, 0, -1):
+            x = self.snakes[snake - 1].xcor()  # Get the x coordinate of the previous snake
+            y = self.snakes[snake - 1].ycor()  # Get the y coordinate of the previous snake
+            self.snakes[snake].goto(x, y)  # Move the current snake to the previous snake's position
+        self.snakes[0].forward(20)
+
+    def up(self):
+        """
+        Move the snake up by 20 pixels
+        :return:
+        """
+        if self.snakes[0].heading() != 270:
+            self.snakes[0].setheading(90)
+
+    def down(self):
+        """
+        Move the snake down by 20 pixels
+        :return:
+        """
+        if self.snakes[0].heading() != 90:
+            self.snakes[0].setheading(270)
+
+    def left(self):
+        """
+        Move the snake left by 20 pixels
+        :return:
+        """
+        if self.snakes[0].heading() != 0:
+            self.snakes[0].setheading(180)
+
+    def right(self):
+        """
+        Move the snake right by 20 pixels
+        :return:
+        """
+        if self.snakes[0].heading() != 180:
+            self.snakes[0].setheading(0)
+
+    def refresh(self):
+        """
+        Refreshes the screen to show the snake moving forward and delay for 0.1 seconds each time the loop runs
+        :return:
+        """
+        screen = Screen()
+        screen.update()
+        time.sleep(.1)
+        self.move()
